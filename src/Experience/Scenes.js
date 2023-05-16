25c66e701f629b6d998b28912f9fe3e90ab4f3dc
Jasmeet-Chicmic <124142185+Jasmeet-Chicmic@users.noreply.github.com>
2023-05-16 12:39:48 +0530

    Delete src directory
    delete

--- src/Experience/Scenes.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three"
-import Experience from "./Experience";
-export default class Scenes{
-    constructor(name){
-        this.scene = new THREE.Scene();
-        this.scene.name = name;
-        this.experience = new Experience();
-    }
-}
